6ce317dcd983f5ea588b0da49b66d7ed005f5220
Henrik Joreteg <henrik@joreteg.com>
2024-09-30 13:35:44 -0700

    updated: presorting for more faster validation checks

--- src/basic-types.js
+++ src/basic-types.js
@@ -1,10 +1,10 @@
 const str = val => typeof val === 'string'
 const bool = val => typeof val === 'boolean'
 const num = val => typeof val === 'number'
-const int = val => num(val) && Math.round(val) === val
+const int = Number.isInteger
 const positiveInt = val => int(val) && val >= 0
 const matches = values => val => values.includes(val)
-const timestamp = val => !isNaN(new Date(val).valueOf())
+const timestamp = val => positiveInt(val) && val > 0 && val < 7258147200000 // this is year 2200
 const arr = Array.isArray
 const obj = val => typeof val === 'object'
 const func = val => typeof val === 'function'
